548ee9d9ad231be1873b63770461f6fb6299550e
LucasSaller <sallerlucas@gmail.com>
2021-10-07 18:54:50 -0300

    Cambio un poco de disenio y agrego unos links para posibles futuras pantallas

--- src/components/NavBar.js/NavBar.js
+++ src/components/NavBar.js/NavBar.js
@@ -1,85 +1,169 @@
-import React from 'react'
-import { useTheme } from '@mui/material/styles';
-import { makeStyles } from '@mui/styles';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Menu from '@mui/material/Menu';
 import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
+import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import logo from '../../assets/logo.png';
+import { makeStyles } from '@mui/styles';
+
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-const useStyles = makeStyles({
-  loginButton: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 0,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    padding: '0 30px',
-  },
-  menuItem:{  
-      color: '#fff',
-      display:'flex', 
-      gap:20,
-      listStyle:'none',
-      "& a":{
-
-        textDecoration: 'none',
-      },
-      '& *:visited':{
-        color: 'white',
-        textDecoration: 'none',
-      }
-  }
-});
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
   
+const useStyles = makeStyles({
+  icon:{
+      color:'#fff'
+    
+  },
+    logo:{
+      width:150,
+      height:100,
+      objectFit:'contain',
+    },
+    loginButton: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 0,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      padding: '0 30px',
+    },
+    menuItem:{  
+        color: '#fff',
+        display:'flex', 
+        gap:20,
+        listStyle:'none',
+        alignItems: 'center',
+        "& a":{
+  
+          textDecoration: 'none',
+        },
+        '& *:visited':{
+          color: 'white',
+          textDecoration: 'none',
+        }
+    }
+   
+  });
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 'auto',
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
 export default function NavBar() {
-  const classes = useStyles();
+    const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const theme = useTheme();
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
   };
+
+  const menuId = 'primary-search-account-menu';
+  
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >   
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={0} color="error">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+        <p>Cart</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={1} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <Router>
-
+      <Router>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar style={{ justifyContent: 'space-between'}}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            
-            >
-            <ShoppingCartIcon />
-          </IconButton>
-          <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        {/* <Typography variant='body' sx={{ minWidth: 100 }} className={classes.menuItem} >Contact</Typography>
-        <Typography variant='body' sx={{ minWidth: 100 }} className={classes.menuItem} >Profile</Typography> */}
+        <Toolbar>
+        <img alt='logo'src={logo} className={classes.logo}/>
         <nav>
           <ul className={classes.menuItem}>
             <li>
@@ -88,45 +172,74 @@ export default function NavBar() {
             <li>
               <Link to="/products">Products</Link>
             </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
+           
           </ul>
         </nav>
-      </Box>
-      
-          <Button variant='contained' color='grey' className={classes.loginButton}>Login</Button>
-          {/* <Tooltip title="Account settings">
-          <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
-          <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-          </IconButton>
-        </Tooltip> */}
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <Link to="/cart" className={classes.icon}> 
+            <IconButton size="large" aria-label="mails" color="inherit">
+              <Badge badgeContent={0} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+            </Link>
+            <IconButton
+              size="large"
+              aria-label="notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={1} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
     </Box>
     <Switch>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        </Router>
+    <Route path="/products">
+      <Products />
+    </Route>
+    <Route path="/cart">
+      <Cart />
+    </Route>
+    <Route path="/">
+      <Home />
+    </Route>
+  </Switch>
+  </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Products() {
-  return <h2>Products</h2>;
-}
-
-function Cart() {
-  return <h2>Cart</h2>;
+  function Home() {
+    return <h2>Home</h2>;
+  }
+  
+  function Products() {
+    return <h2>Products</h2>;
+  }
+  
+  function Cart() {
+    return <h2>Cart</h2>;
+  }
 }
