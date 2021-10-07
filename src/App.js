624e63da040fc2954499a33e00050d71fe76d96c
LucasSaller <sallerlucas@gmail.com>
2021-10-07 13:11:04 -0300

    Agrego links al nav y agrego estilos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js/NavBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+let theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#31adc5',
+      contrastText: '#FFFFFF',
+    },
+    primary: {
+      main: '#00204a',
+      contrastText: '#FFFFFF',
+    },
+    accent: {
+      main: '#2b78ff',
+    },
+    progress: {
+      green: '#4dd63b',
+      yellow: '#ffc700',
+      orange: '#ff9345',
+    },
+    state: {
+      success: '#56e041',
+      failure: '#ff3a5e',
+    },
+    grey: {
+      main: '#75808f',
+      light: '#d2d7da',
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: ['"Poppins"', 'sans-serif'].join(','),
+    useNextVariants: true,
+    button: {
+      textTransform: 'none',
+    },
+    allVariants: {
+      color: '#22282f',
+    },
+  },
+
+});
+
 
 function App() {
+  
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
      <section> 
        <NavBar />
      </section>
     </div>
+    </ThemeProvider>
+    
   );
 }
 
--- src/components/NavBar.js/NavBar.js
+++ src/components/NavBar.js/NavBar.js
@@ -1,70 +1,132 @@
 import React from 'react'
-
+import { useTheme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Menu from '@mui/material/Menu';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
 import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+const useStyles = makeStyles({
+  loginButton: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 0,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    padding: '0 30px',
+  },
+  menuItem:{  
+      color: '#fff',
+      display:'flex', 
+      gap:20,
+      listStyle:'none',
+      "& a":{
+
+        textDecoration: 'none',
+      },
+      '& *:visited':{
+        color: 'white',
+        textDecoration: 'none',
+      }
+  }
+});
   
 export default function NavBar() {
-
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const theme = useTheme();
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
+    <Router>
+
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar style={{ justifyContent: 'space-between'}}>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-            onClick={handleClick}
-
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Ecommerce
-          </Typography>
-          <IconButton 
-            size='large' edge="start"
-            color="inherit"
-            aria-label="menu"
+            
             >
-
             <ShoppingCartIcon />
           </IconButton>
-          <Button color="inherit">Login</Button>
+          <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        {/* <Typography variant='body' sx={{ minWidth: 100 }} className={classes.menuItem} >Contact</Typography>
+        <Typography variant='body' sx={{ minWidth: 100 }} className={classes.menuItem} >Profile</Typography> */}
+        <nav>
+          <ul className={classes.menuItem}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>
+      </Box>
+      
+          <Button variant='contained' color='grey' className={classes.loginButton}>Login</Button>
+          {/* <Tooltip title="Account settings">
+          <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+          <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+          </IconButton>
+        </Tooltip> */}
         </Toolbar>
       </AppBar>
     </Box>
+    <Switch>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </Router>
   );
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Products() {
+  return <h2>Products</h2>;
+}
+
+function Cart() {
+  return <h2>Cart</h2>;
+}
