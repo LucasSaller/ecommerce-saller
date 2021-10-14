3f835f3ca3dae1bdeca3d03c866db439da8ee912
Lucas Saller <sallerlucas@gmail.com>
2021-10-14 20:47:04 -0300

    Arreglo el desafio de Promise y Map que no funcionaba

--- src/components/Items/ItemList.js
+++ src/components/Items/ItemList.js
@@ -2,15 +2,22 @@ import React from "react";
 import Item from "./Item";
 import { Grid } from "@mui/material";
 function ItemList({ items }) {
-  console.log(items);
   return (
     <div>
-      {items &&
-        items.map((item) => (
-          <Grid item xs={12} md={4} style={{ marginBottom: 100 }} key={item.id}>
-            <Item item={item} />
-          </Grid>
-        ))}
+      <Grid container>
+        {items &&
+          items.map((item) => (
+            <Grid
+              item
+              xs={12}
+              md={4}
+              style={{ marginBottom: 100 }}
+              key={item.id}
+            >
+              <Item item={item} />
+            </Grid>
+          ))}
+      </Grid>
     </div>
   );
 }
--- src/components/Items/ItemListContainer.js
+++ src/components/Items/ItemListContainer.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "../../dummy/data.json";
 import { Grid } from "@mui/material";
 import "./ItemListContainer.css";
 import ItemList from "./ItemList";
 
 function ItemListContainer({ greeting }) {
-  let items = [];
-  const task = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data.items);
-    }, 3000);
-  });
-  task.then(
-    (result) => {
-      items = result;
-      console.log(result);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const task = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(data.items);
+      }, 3000);
+    });
+    task.then(
+      (result) => {
+        console.log(result);
+        setProducts(result);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+
   return (
     <div>
       <h2 style={{ textAlign: "center", margin: "30px 0" }}>{greeting}</h2>
       <Grid container className="container__products">
-        <ItemList items={items} />
+        <ItemList items={products} />
       </Grid>
     </div>
   );
